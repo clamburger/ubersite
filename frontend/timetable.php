77ec9b8a3764535c90cbbd8cf1d5908887b46e95
Douglas Catchpole <douglas.catchpole@gmail.com>
2012-09-03 11:30:55 +1000

    Modify the way timetable layout is generated, no more inaccurate row spans. Sessions less than 20 minutes should be avoided, or perhaps accounted for.

--- frontend/timetable.php
+++ frontend/timetable.php
@@ -14,42 +14,68 @@
 
     # Create the timetable table with the times along the left.
     $days = array();
+    $SLEEP_TIME = 23;
+    $AWAKE_TIME = 6;
+    $timetable = array_fill(0, ($SLEEP_TIME-$AWAKE_TIME+0.5)*2, Array());
     foreach ($CAMP_DAYS as $day) {
       $days[$day] = count($days)+1;
+      $timetable[0][$days[$day]] = "<td rowspan='35'>";
     }
     $tpl->set('days', $CAMP_DAYS);
 
-    $SLEEP_TIME = 23;
-    $AWAKE_TIME = 6;
 
     $count = 0;
     $old = 0;
     $oldstart = "";
-    $timetable = array_fill(0,($SLEEP_TIME-$AWAKE_TIME+0.5)*12,Array());
     for ($i = 0; $i < count($timetable); $i++) {
-      if (($i%6)==0) {
-        $timetable[$i][] = "<th rowspan='6'>".intval(6+($i/12)).":".sprintf("%02d",($i%12)*5)."</th>";
-      }
+      $timetable[$i][0] = "<th>".intval(6+($i/2)).":".sprintf("%02d",($i%2)*30)."</th>";
     }
 
-    # Black magic that figures out the duration of each event and gives each cell the correct contents and height.
+    # Black magic that figures out the duration of each event and gives each
+    # cell the correct contents and height.
     $hour = date("H");
     $time = date(":i");
     $time = (intval($hour)) . $time;
     while ($row = fetch_row($res)) {
-      $duration = (intval(substr($row['End'],0,2))-intval(substr($row['Start'],0,2)))*12 + ((intval(substr($row['End'],3,2))-intval(substr($row['Start'],3,2)))/5);
-      $duration = intval($duration);
-      # If the day matches, the time is after the start time and the time is before the end time the event is active.
+      $last = false;
+      $startMin = intval(substr($row['Start'],0,2)) * 60 + intval(
+          substr($row['Start'],3,2));
+      if ($startMin < $AWAKE_TIME*60) {
+        $startMin = $AWAKE_TIME*60;
+      }
+      $endMin = intval(substr($row['End'],0,2)) * 60 + intval(
+          substr($row['End'],3,2));
+      if ($endMin > $SLEEP_TIME*60) {
+        $endMin = $SLEEP_TIME*60 + 29;
+        $last = true;
+      }
+      $duration = ($endMin - $startMin);
+      # If the day matches, the time is after the start time and the time is
+      # before the end time the event is active.
       $class = "";
-      if ($row["Day"] == date("l") && strtotime($time) >= strtotime($row["Start"]) && strtotime($time) < strtotime($row["End"]) && !$wget) {
+      if ($row["Day"] == date("l") &&
+          strtotime($time) >= strtotime($row["Start"]) &&
+          strtotime($time) < strtotime($row["End"]) && !$wget) {
         $class = " class='active'";
       }
 
-      $arrayKey = intval(substr($row['Start'],0,2)-6)*12 + intval(substr($row['Start'],3,2))/5;
-      $timetable[$arrayKey][$days[$row["Day"]]] = "<td rowspan=$duration$class>${row['Activity']}</td>";
+      $arrayKey = ($startMin - 6*60) / 5;
+      $always = 2;
+      #$always -= ($duration / 30) * 2;
+      $height = $duration - $always;
+      $half = intval(($height - 18) / 2);
+      $height -= $half;
+
+      $timetable[0][$days[$row["Day"]]] .=
+          "<div style=\"padding:${half}px 0 0 0;height:${height}px;" .
+          ($last ? "border-bottom:0;" : "") .
+          "\"$class>${row['Activity']}</div>";
 
       $old = $row['Day'];
       $oldstart = $row['Start'];
+    }
+    foreach ($days as $day => $v) {
+      $timetable[0][$v] .= "</td>";
     }
     $tpl->set('timetable', $timetable);
   }
--- templates/timetable.tpl
+++ templates/timetable.tpl
@@ -3,23 +3,23 @@
 Here is the planned timetable for the camp. Actual schedule may vary!<br/>
 <br/>
 <table class="timetable">
-    <tr>
-        <th width="5%">Time</th>
-        <loop:days>
-			<th width="16%"><tag:days[] /></th>
-		</loop:days>
-    </tr>
-    <loop:timetable>
-        <tr>
-            <tag:timetable[].0 />
-            <tag:timetable[].1 />
-            <tag:timetable[].2 />
-            <tag:timetable[].3 />
-            <tag:timetable[].4 />
-            <tag:timetable[].5 />
-            <tag:timetable[].6 />
-        </tr>
-    </loop:timetable>
+  <tr>
+    <th width="5%">Time</th>
+    <loop:days>
+    <th width="16%"><tag:days[] /></th>
+    </loop:days>
+  </tr>
+  <loop:timetable>
+  <tr>
+    <tag:timetable[].0 />
+    <tag:timetable[].1 />
+    <tag:timetable[].2 />
+    <tag:timetable[].3 />
+    <tag:timetable[].4 />
+    <tag:timetable[].5 />
+    <tag:timetable[].6 />
+  </tr>
+  </loop:timetable>
 </table>
 <else:timetable>
 No timetable data currently exists.
