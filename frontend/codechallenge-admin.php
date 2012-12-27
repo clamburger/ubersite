75deaad982025c5b3571909224735e6badd6ca6a
Stephanie Zylstra <stephanie.zylstra@uqconnect.edu.au>
2012-12-27 14:28:03 +1000

    Added zip download for all rsults

--- frontend/codechallenge-admin.php
+++ frontend/codechallenge-admin.php
@@ -12,6 +12,33 @@
     $messages->addMessage(new Message("success", "All test cases have been made visible."));
 
   }
+  
+  function downloadAll($path, $zipname) {
+	$zip = new ZipArchive;
+	if ( ($dir = opendir($path)) !== false ) {
+		$zip->open($zipname, ZipArchive::CREATE);
+
+         while ( ($file = readdir($dir)) !== false ) {
+             if ($file != '.' && $file != '..') {
+             	$zip->addFile($file);
+             }
+         }
+     } else {
+         return false;
+     }
+	 $zip->close();
+	 return true;
+  }
+  
+  if (isset($_POST['downloadZip'])) {
+    $zipname = "submissions.zip";
+    if (downloadAll("camp-data/uploads/codechallenge", $zipname)) {
+      header('Content-Type: application/zip');
+      header('Content-disposition: attachment; filename=' . $zipname);
+      header('Content-Length: ' . filesize($zipname));
+      readfile($zipname);
+    }
+  }
 
   $tpl->set('previous', false, true);
 
